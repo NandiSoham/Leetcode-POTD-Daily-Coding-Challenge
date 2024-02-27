7f8db0f4676e12f1b6c9ff40deaa3ac204832be9
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-02-27 02:48:37 -0800

    Feb Day - 27 solved: Diameter of Binary Tree

---
+++ February/day-27_diameterOfBinaryTree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+
+private:
+    int height(TreeNode* node, int& diameter) {
+
+        if (!node) {
+            return 0;
+        }
+
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+// Problem Link -> https://leetcode.com/problems/diameter-of-binary-tree/
