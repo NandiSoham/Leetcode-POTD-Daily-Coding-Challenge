c76cf296523783f512a5c4a0bc056182b4263d58
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-02 01:51:15 -0800

    March Day - 02 solved: Squares of a Sorted Array ( in optimal approach )

--- March/day_02-977_squaresOfASortedArray.cpp
+++ March/day_02-977_squaresOfASortedArray.cpp
@@ -17,6 +17,35 @@ public:
 //Space Complexity : O(1)
 //------------------------------------------------------------------------------------------
 
+// ------------------------------------- Approach - 2 --------------------------------------
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int i = 0, j  = n - 1, k = n - 1;
+
+        while(k >= 0){
+            int a = nums[i] * nums[i];
+            int b = nums[j] * nums[j];
+
+            if(a > b){
+                result[k] = a;
+                i++;
+            } else{
+                result[k] = b;
+                j--;
+            }
+            k--;
+        }
+        return result;
+    }
+};
 
 
 
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+// Problem Link -> https://leetcode.com/problems/squares-of-a-sorted-array/description/
