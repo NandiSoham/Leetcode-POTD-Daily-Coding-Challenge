d2e97c5b60fbf4ad9214071786058bce71bf5a2c
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-09 02:00:26 -0800

    March Day - 09 solved: Minimum Common Value ( Approach-2 )
    using binary search

--- March/day_09-2540_minimumCommonValue.cpp
+++ March/day_09-2540_minimumCommonValue.cpp
@@ -17,4 +17,45 @@ public:
 // Time Complexity -> O(n + m)
 // Space Complexity -> O(n)
 
-//--------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+//---------------------------------- APPROACH - 2 ----------------------------
+
+class Solution {
+public:
+    bool binarySearch(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            
+            if(nums[mid] == target) {
+                return true;
+            } else if(nums[mid] < target) {
+                left = mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        
+        return false;
+    }
+    
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        for(int &num : nums1) {
+            if(binarySearch(nums2, num))
+                return num;
+        }
+        
+        return -1;
+    }
+};
+
+
+
+// Time Complexity -> O(nlogm)
+// Space Complexity -> O(1)
+
+//----------------------------------------------------------------------------
+
