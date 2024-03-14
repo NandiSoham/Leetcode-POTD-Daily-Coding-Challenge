2f1fb0d8dac74c114b27f60114f7a2ebb6f83c31
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-14 16:52:01 -0700

    March Day - 14 solved: Binary Subarrays With Sum ( Sliding Window approach )

--- March/day_14-930_binarySubarraysWithSum.cpp
+++ March/day_14-930_binarySubarraysWithSum.cpp
@@ -1,3 +1,7 @@
+// Problem Link -> https://leetcode.com/problems/binary-subarrays-with-sum/description/
+
+//------------------------- APPROACH - 1 ( Prefix Sum)-------------------------------
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
@@ -25,3 +29,46 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(n)
+
+// --------------------------------------------------------------------------------------
+
+//------------------------- APPROACH - 2 ( Sliding Window )-----------------------------------
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int prefix_zeros = 0;
+        int window_sum = 0;
+        int count = 0;
+        
+        int i = 0, j = 0;
+        
+        while(j < nums.size()) {
+            window_sum += nums[j];
+            
+            while (i < j && (nums[i] == 0 || window_sum > goal)) {
+                if (nums[i] == 1) {
+                    prefix_zeros = 0;
+                } else {
+                    prefix_zeros += 1;
+                }
+                
+                window_sum -= nums[i];
+                i++;
+            }
+            
+            if (window_sum == goal) {
+                count += 1 + prefix_zeros;
+            }
+            j++;
+        }
+        
+        return count;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+//-------------------------------------------------------------------------------------
