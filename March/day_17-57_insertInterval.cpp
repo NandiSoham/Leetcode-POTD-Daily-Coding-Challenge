f09f8a867ef7f840d7ae46c16306588d488b9d93
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-17 03:30:11 -0700

    March Day - 17 solved: Insert Interval (Optimal Approach)

--- March/day_17-57_insertInterval.cpp
+++ March/day_17-57_insertInterval.cpp
@@ -28,3 +28,39 @@ public:
 // Space Complexity -> O(1)
 
 // -------------------------------------------------------------------------------------------------------------
+
+// -------------------------------------- Optimal Approach --------------------------------------------
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        vector<vector<int>> ans;
+
+        for (i = 0; i < intervals.size(); i++) {
+            if (intervals[i][1] < newInterval[0])
+                ans.push_back(intervals[i]);
+            else if (intervals[i][0] > newInterval[1])
+                break;
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+        ans.push_back(newInterval);
+
+        while(i < intervals.size()){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        return ans;
+        
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+//-------------------------------------------------------------------------------------------------------
