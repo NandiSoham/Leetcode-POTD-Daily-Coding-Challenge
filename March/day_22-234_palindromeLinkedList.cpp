42e9b32919732ff0d2280761676f46d98b010a7d
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-22 01:04:16 -0700

    March Day - 22 solved: Palindrome Linked List ( Brute Force Approach)

--- March/day_22-234_palindromeLinkedList.cpp
+++ March/day_22-234_palindromeLinkedList.cpp
@@ -28,3 +28,58 @@ public:
 // Space Complexity -> O(n)
 
 //----------------------------------------------------------------------------------------------------
+
+// ------------------------------------- Approach - 2 (Better Approach) ------------------------------
+
+class Solution {
+public:
+
+    ListNode* reverseLinkedList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+
+        while(temp != NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        // Check if the linked list is empty or has only one node
+        if (head == NULL || head->next == NULL) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse the second half of the linked list starting from the middle
+        ListNode* newHead = reverseLinkedList(slow->next);
+
+        ListNode* left = head;
+        ListNode* right = newHead;
+        while(right != NULL){
+            if(left->val != right->val){
+                // Reverse the second half back to its original state
+                reverseLinkedList(newHead); 
+                return false;
+            }
+            left = left->next;
+            right = right->next;
+        }
+        // Reverse the second half back to its original state
+        reverseLinkedList(newHead);
+        return true;
+    }
+};
+
+
+// Time Complexity -> O(2n)
+// Space Complexity -> O(1)
+
+//----------------------------------------------------------------------------------------------------
