455854361202ff6191bf01f46776d41ec3a80c34
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-23 11:51:04 -0700

    March Day - 23 solved: Reorder List ( Approach - 2 using extra space )

--- March/day_23-143_reorderList.cpp
+++ March/day_23-143_reorderList.cpp
@@ -1,3 +1,7 @@
+// Probelm Link -> https://leetcode.com/problems/reorder-list/
+
+//--------------------------------- Approach - 1 ---------------------------------------
+
 class Solution {
 public:
 
@@ -38,3 +42,50 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
+
+//--------------------------------------------------------------------------------------
+
+//----------------------- Approach - 2 (using extra space) -----------------------------
+
+class Solution {
+public:
+
+    ListNode* revLinkedList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+
+        ListNode* last = revLinkedList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }   
+
+    void reorderList(ListNode* head) {        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* rev = revLinkedList(slow);
+        ListNode* curr = head;
+
+        while(rev->next != NULL){
+            ListNode* currtemp = curr->next;
+            curr->next = rev;
+
+            ListNode* rev_temp = rev->next;
+            rev->next = currtemp;
+
+            curr = currtemp;
+            rev = rev_temp;
+        }
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+//--------------------------------------------------------------------------------------
