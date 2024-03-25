57ba595955902f04c846888b49eeb0e74fff2fb5
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-24 22:18:06 -0700

    March Day - 24 solved: Find the Duplicate Number ( Sort and Search )

--- March/day_24-287_findTheDuplicateNumber.cpp
+++ March/day_24-287_findTheDuplicateNumber.cpp
@@ -1,3 +1,7 @@
+// Problem Link -> https://leetcode.com/problems/find-the-duplicate-number/
+
+// ------------------- Approach 1 -> Implemented Tortoise and Hare Algorithm -------------------------
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -26,4 +30,29 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
-// Problem Link -> https://leetcode.com/problems/find-the-duplicate-number/
+
+// ---------------------------------------------------------------------------------------------------
+
+// ---------------------------- Approach - 2 -> Sort and search iteratively --------------------------
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int prev = -1;
+        for (int num : nums) {
+            if (num == prev)
+                break;
+            prev = num;
+        }
+
+        return prev;
+        }
+};
+
+
+
+// Time Complexity -> O(n long)
+// Space Complexity -> O(1)
+// ---------------------------------------------------------------------------------------------------
