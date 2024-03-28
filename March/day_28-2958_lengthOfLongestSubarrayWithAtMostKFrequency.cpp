e5d5f12f4bab1d2621b23b3d1da869ea5c10a4cd
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-03-28 01:52:30 -0700

    March Day - 27 solved: Length of Longest Subarray With at Most K Frequency Approach -2 )
    using sliding window without nested loop

--- March/day_28-2958_lengthOfLongestSubarrayWithAtMostKFrequency.cpp
+++ March/day_28-2958_lengthOfLongestSubarrayWithAtMostKFrequency.cpp
@@ -30,3 +30,39 @@ public:
 // Space Complexity -> O(n)
 
 //-------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------- Approach - 1 -> (sliding window without nested loop) -----------------------------
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+
+        int left = 0, right = 0;
+        int ansLength = 0;
+        int culprit = 0;
+
+        while(right < n){
+            mpp[nums[right]]++;
+            if(mpp[nums[right]] == k+1) culprit++;
+
+            if(culprit > 0){
+                mpp[nums[left]]--;
+                if(mpp[nums[left]] == k) culprit--;
+                left++;
+            }
+            if(culprit == 0){
+                ansLength = max(ansLength, right - left + 1);   
+            }
+            right++;
+        }
+        return ansLength;
+    }
+};
+
+
+// Time COmplexity -> O(n)
+// Space Complexity -> O(n)
+
+//-------------------------------------------------------------------------------------------------------------------
