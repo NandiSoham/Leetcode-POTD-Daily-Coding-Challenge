4c650f806ade72b166f8fc78d75bb1b495d26c22
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-04-17 23:10:59 -0700

    April Day - 04 solved: 988. Smallest String Starting From Leaf ( 2nd approach )
    using BFS

--- April/day_17-988_Smallest_String_Starting_From_Leaf.cpp
+++ April/day_17-988_Smallest_String_Starting_From_Leaf.cpp
@@ -1,3 +1,8 @@
+// Problem Link -> https://leetcode.com/problems/smallest-string-starting-from-leaf/description/
+
+// ---------------------------------------- Approach - 1 (Using DFS) ---------------------------------------
+
+
 class Solution {
 public:
 
@@ -28,4 +33,47 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
-// Problem Link -> https://leetcode.com/problems/smallest-string-starting-from-leaf/description/
+
+// --------------------------------------------------------------------------------------------------------
+
+// ---------------------------------------- Approach - 2 (Using BFS) ---------------------------------------
+
+class Solution {
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        queue<pair<TreeNode*, string>> queOfNode;
+        
+        string ans = "";
+        queOfNode.push({root, string(1, root->val + 'a')});
+        
+        while(!queOfNode.empty()) {
+            
+            auto[node, currStr] = queOfNode.front();
+            queOfNode.pop();
+            
+            if(node->left == NULL && node->right == NULL) {
+                if(ans == "" || ans > currStr) {
+                    ans = currStr;
+                }
+            }
+            
+            if(node->left) {
+                queOfNode.push({node->left, char(node->left->val + 'a') + currStr});
+            }
+            
+            if(node->right) {
+                queOfNode.push({node->right, char(node->right->val + 'a') + currStr});
+            }
+            
+        }
+        
+        return ans;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+--------------------------------------------------------------------------------------------------------
