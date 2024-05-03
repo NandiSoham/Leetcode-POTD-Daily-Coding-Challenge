74ca4e10961606de3d71a511a9f539f65fd1872c
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-03 01:27:56 -0700

    May Day - 03 solved: 165. Compare Version Numbers (Approach - 2)
    2 pointers approach

--- May/day03-165. Compare Version Numbers.cpp
+++ May/day03-165. Compare Version Numbers.cpp
@@ -23,7 +23,7 @@ public:
         int m = second_version_tokens.size();
         int i = 0;
 
-        while(i < n || i < m){
+        while(i < n || j < m){
             int num1 = i < n ? stoi(first_version_tokens[i]) : 0;
             int num2 = i < m ? stoi(second_version_tokens[i]) : 0;
 
@@ -39,5 +39,44 @@ public:
 
 // Time Complexity -> O(n+m)
 // Space Complexity -> O(n+m)
+
+//-----------------------------------------------------------------------------------------------
+
+// ---------------------------------------- Approach - 2  ---------------------------------------
+
+class Solution {
+public:
+
+    int compareVersion(string version1, string version2) {
+        int n = version1.size();
+        int m = version2.size();
+        int i = 0, j = 0;
+
+        while(i < n || j < m){
+            int num1 = 0;
+            int num2 = 0;
+
+            while(i < n && version1[i] != '.'){
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            while(j < m && version2[j] != '.'){
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if(num1 > num2) return 1;
+            else if(num1 < num2) return -1;
+            else i++, j++;
+        }
+        return 0;
+    }
+};
+
+
+
+// Time Complexity -> O(max(n,m)
+// Space Complexity -> O(1)
 
 //-----------------------------------------------------------------------------------------------
