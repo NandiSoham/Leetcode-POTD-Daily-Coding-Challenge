bc5dd6a296b5599ef2cdde3a9f49a5cdda9e3a98
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-14 01:57:36 -0700

    different way of code writing for 1219. Path with Maximum Gold

--- May/day14-1219. Path with Maximum Gold.cpp
+++ May/day14-1219. Path with Maximum Gold.cpp
@@ -46,3 +46,44 @@ public:
 
 // Time Complexity -> O(m * n * 4^k), where m and n are the dimensions of the grid, and k is the number of cells containing gold. 
 // Space Complexty ->  O(m * n)
+
+
+// ------------------------------ Same approach with different way of writing ----------------------------------------------------
+
+class Solution {
+public:
+    int m, n;
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if (i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == 0) {
+            return 0;
+        }
+
+        int originalGoldValue = grid[i][j];
+        grid[i][j] = 0;
+
+        int maxGold = 0;
+        maxGold = max(maxGold, dfs(grid, i - 1, j)); // up
+        maxGold = max(maxGold, dfs(grid, i + 1, j)); // down
+        maxGold = max(maxGold, dfs(grid, i, j - 1)); // left
+        maxGold = max(maxGold, dfs(grid, i, j + 1)); // right
+
+        grid[i][j] = originalGoldValue;
+        return originalGoldValue + maxGold;
+    }
+
+    int getMaximumGold(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        int maxGold = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0) {
+                    maxGold = max(maxGold, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxGold;
+    }
+};
