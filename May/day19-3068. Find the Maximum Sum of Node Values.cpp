19b75f1e5223ea0a5bb15f316a7c514ea8e3da86
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-19 21:10:53 -0700

    May Day - 19 solved: 3068. Find the Maximum Sum of Node Values (Approach - 2)

--- May/day19-3068. Find the Maximum Sum of Node Values.cpp
+++ May/day19-3068. Find the Maximum Sum of Node Values.cpp
@@ -36,3 +36,36 @@ public:
 // Space Complexity -> O(1)
 
 //-----------------------------------------------------------------------------------------------
+
+// ---------------------------------------- Approach - 2 ---------------------------------------
+
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        vector<int> profit;
+        long long currSum = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            currSum += nums[i];
+            int xorVal = nums[i] ^ k;
+            profit.push_back(xorVal - nums[i]);
+        }
+
+        sort(profit.begin(), profit.end(), greater<int>());
+
+        for(int i = 0; i < profit.size() - 1; i += 2){
+            long long pairSum = profit[i] + profit[i+1];
+
+            if(pairSum > 0){
+                currSum += pairSum;
+            }
+        }
+        return currSum;
+    }
+};
+
+
+// Time Complexity -> O(n long)
+// Space Complexity -> O(n)
+
+//-----------------------------------------------------------------------------------------------
