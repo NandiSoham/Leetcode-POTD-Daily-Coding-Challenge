735cc2ba207f98d071da4c1624103f6595796cc6
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-20 00:52:44 -0700

    May Day - 20 solved: 1863. Sum of All Subset XOR Totals (Better approach)

--- May/day20-1863. Sum of All Subset XOR Totals.cpp
+++ May/day20-1863. Sum of All Subset XOR Totals.cpp
@@ -40,3 +40,31 @@ public:
 // Space Complexity -> O(n * 2^n)
 
 //-----------------------------------------------------------------------------------------------
+
+// ---------------------------------------- Better Approach ---------------------------------------
+
+class Solution {
+public:
+    int solve(vector<int>& nums, int i, int xorVal) {
+
+        if (i == nums.size()) {
+            return xorVal;
+        }
+
+        int inc = solve(nums, i + 1, nums[i] ^ xorVal);
+        int exc = solve(nums, i + 1, xorVal);
+
+        return inc + exc;
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        return solve(nums, 0, 0);
+    }
+};
+
+
+
+// Time Complexity -> O(2^n)
+// Space Complexity -> O(n)
+
+//-----------------------------------------------------------------------------------------------
