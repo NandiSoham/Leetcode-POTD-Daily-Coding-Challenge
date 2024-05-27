5725bbc3931d40e29c884841fe868244d20d1814
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-27 00:57:17 -0700

    May Day - 26 solved: 1608. Special Array With X Elements Greater Than or Equal X (Better approach)

--- May/day27-1608. Special Array With X Elements Greater Than or Equal X.cpp
+++ May/day27-1608. Special Array With X Elements Greater Than or Equal X.cpp
@@ -21,3 +21,35 @@ public:
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(1)
 // -------------------------------------------------------------------------------------------
+
+// ---------------------------- Approch - 2 ( Sorting and Binary Search ) ----------------------------------
+
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int low = 0, high = n;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            
+            for(int i = 0; i < n; i++){
+                if(nums[i] >= mid) count++;
+            }
+            
+            if(count == mid) return mid;
+            else if(count < mid) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+};
+
+
+
+// Time Complexity -> O(nlogn)
+// Space Complexity -> O(1)
+
+// -------------------------------------------------------------------------------------------------------------
