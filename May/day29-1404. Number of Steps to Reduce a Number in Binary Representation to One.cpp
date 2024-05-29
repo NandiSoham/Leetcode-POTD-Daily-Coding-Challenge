ea1066cd1a542ae1c9cee0dc09c532c22e9a49cc
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-29 02:04:16 -0700

    May Day - 29 solved: 1404. Number of Steps to Reduce a Number in Binary Representation to One (Approach - 2)

--- May/day29-1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
+++ May/day29-1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -1,5 +1,6 @@
 // Problem Link -> https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
 
+// ------------------------------------------ Approach - 1 --------------------------------------------
 class Solution {
 public:
     int numSteps(string s) {
@@ -32,3 +33,32 @@ public:
 
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(1)
+
+// ----------------------------------------------------------------------------------------------------
+
+// ------------------------------------------ Approach - 2 --------------------------------------------
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int operationCount = 0;
+        int carry = 0;
+
+        for(int i = s.length() - 1; i >= 1; i--) {
+            if(((s[i] - '0') + carry) % 2 == 1) { //odd
+                operationCount += 2;
+                carry = 1;
+            } else { //even
+                operationCount += 1;
+            }
+        }
+
+        return operationCount + carry;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ----------------------------------------------------------------------------------------------------
