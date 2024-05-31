ded47d8f635edd108676707140fdb043fef63a0e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-05-31 02:26:44 -0700

    May Day - 31 solved: 260. Single Number III (Optimal)

--- May/day31-260. Single Number III.cpp
+++ May/day31-260. Single Number III.cpp
@@ -25,3 +25,34 @@ public:
 // Space Complexity -> O(1)
 
 //----------------------------------------------------------------------------------------------
+
+// ---------------------------------------- Optimal Approach ----------------------------------------
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long overallXor = 0;
+        for(int i = 0; i < nums.size(); i++){
+            overallXor = overallXor ^ nums[i];
+        }
+
+        long long rightmostBit = (overallXor ^ (overallXor - 1)) & overallXor;
+        int firstBucket = 0, secondBucket = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            if((nums[i] & rightmostBit) != 0)
+                firstBucket ^= nums[i];
+            
+            else
+                secondBucket ^= nums[i];
+        }
+        return {static_cast<int>(firstBucket), static_cast<int>(secondBucket)};
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+//----------------------------------------------------------------------------------------------
