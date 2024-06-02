08aadf7b76b6309d2c3d0ca4a9eaefe576fb0053
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-02 01:14:38 -0700

    June Day - 02 solved: 344. Reverse String (Approach-1)

---
+++ June/day02-344. Reverse String.cpp
@@ -0,0 +1,15 @@
+// Problem Link -> https://leetcode.com/problems/reverse-string/description/
+
+// ------------------------------------ Approach - 1 ----------------------------------
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        return reverse(s.begin(), s.end());
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+// ------------------------------------------------------------------------------------
