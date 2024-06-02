cf5d40e86cbf91a9611f29c391a51edff3acd699
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-02 01:15:50 -0700

    June Day - 02 solved: 344. Reverse String (Approach-2)

--- June/day02-344. Reverse String.cpp
+++ June/day02-344. Reverse String.cpp
@@ -13,3 +13,24 @@ public:
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
 // ------------------------------------------------------------------------------------
+
+// ------------------------------------ Approach - 2 ----------------------------------
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int leftPtr = 0;
+        int rightPtr = s.size() - 1;
+
+        while(leftPtr <= rightPtr){
+            swap(s[leftPtr], s[rightPtr]);
+            leftPtr++;
+            rightPtr--;
+        }
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+// ------------------------------------------------------------------------------------
