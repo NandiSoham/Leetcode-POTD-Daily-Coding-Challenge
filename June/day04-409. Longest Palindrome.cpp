0f524b4c5edc81e9b7e594646ff8ce778080f33b
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-04 21:15:39 -0700

    June Day - 04 solved: 409. Longest Palindrome (Approach - 2)

--- June/day04-409. Longest Palindrome.cpp
+++ June/day04-409. Longest Palindrome.cpp
@@ -1,5 +1,7 @@
 // Problem Link -> https://leetcode.com/problems/longest-palindrome/description/
 
+// ------------------------------- APPROACH -1 --------------------------
+
 class Solution {
 public:
     int longestPalindrome(string s) {
@@ -25,3 +27,42 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(n)
+
+//-----------------------------------------------------------------------
+
+
+// ------------------------------- APPROACH - 2 --------------------------
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.length();
+        unordered_map<char, int> mp;
+
+        int result  = 0;
+        int oddFrq  = 0;
+
+        for(char &ch : s) {
+            mp[ch]++;
+
+            if(mp[ch] % 2 != 0) {
+                oddFrq++;
+            } else {
+                oddFrq--;
+            }
+        }
+
+        if(oddFrq > 0) {
+            return n - oddFrq + 1;
+        }
+
+        return n;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+//-----------------------------------------------------------------------
