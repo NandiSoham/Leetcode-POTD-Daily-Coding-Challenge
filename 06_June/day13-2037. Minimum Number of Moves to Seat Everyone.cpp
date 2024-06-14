7964c2d097a1f3cdd0aab14cc3b37bf2172632bc
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-13 22:17:44 -0700

    une Day - 13 solved: 2037. Minimum Number of Moves to Seat Everyone (Approach -2)

--- 06_June/day13-2037. Minimum Number of Moves to Seat Everyone.cpp
+++ 06_June/day13-2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -1,5 +1,7 @@
 // Problem Link -> https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
 
+// ------------------------------------- Approach - 1 --------------------------------------- 
+
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
@@ -19,3 +21,46 @@ public:
 
 // Time Complexity -> O(nlogn)
 // Space Complexity -> O(1)
+
+// ------------------------------------------------------------------------------------------
+
+// ------------------------------------- Approach - 2 --------------------------------------- 
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int n = seats.size();
+        vector<int> seat_position(101, 0);
+        vector<int> student_position(101, 0);
+
+        for (auto& it : seats) {
+            seat_position[it]++;
+        }
+
+        for (auto& it : students) {
+            student_position[it]++;
+        }
+
+        int seat_ptr = 0, stud_ptr = 0;
+        int move_count = 0;
+        while (seat_ptr <= 100 && stud_ptr <= 100) {
+            if (seat_position[seat_ptr] == 0)
+                seat_ptr++;
+            if (student_position[stud_ptr] == 0)
+                stud_ptr++;
+
+            if (seat_ptr <= 100 && stud_ptr <= 100 &&
+                seat_position[seat_ptr] != 0 &&
+                student_position[stud_ptr] != 0) {
+                move_count += abs(seat_ptr - stud_ptr);
+                seat_position[seat_ptr]--;
+                student_position[stud_ptr]--;
+            }
+        }
+
+        return move_count;
+    }
+};
+
+
+// ------------------------------------------------------------------------------------------
