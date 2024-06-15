94df6b85678ce6997bb22a46176db9d6e3605c5b
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-15 16:08:32 -0700

    June Day - 15 solved: 502. IPO

---
+++ 06_June/day15-502. IPO.cpp
@@ -0,0 +1,39 @@
+// Problem Link -> https://leetcode.com/problems/ipo/description/
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital) {
+        int n = profits.size();
+
+        vector<pair<int, int>> vec(n);
+
+        for (int i = 0; i < n; i++) {
+            vec[i] = {capital[i], profits[i]};
+        }
+
+        sort(vec.begin(), vec.end());
+
+        int i = 0;
+
+        priority_queue<int> pq;
+
+        while (k--) {
+            while (i < n && vec[i].first <= w) {
+                pq.push(vec[i].second);
+                i++;
+            }
+            if (pq.empty())
+                break;
+            w += pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
+
+
+
+
+// Time Complexity -> O(nlogn)
+// Space Complexity -> O(n)
