6c37f3f7cd4b502868a1a046bb9ddf5cfbdd2565
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-17 12:57:15 -0700

    June Day - 17 solved: 633. Sum of Square Numbers (Approach-2)

--- 06_June/day17-633. Sum of Square Numbers.cpp
+++ 06_June/day17-633. Sum of Square Numbers.cpp
@@ -23,3 +23,28 @@ public:
 // Time Complexity -> sqrt(c * log(c))
 // Space Complexity -> O(1)
 //-----------------------------------------------------------------------------------
+
+
+// ------------------------------------ Approach - 2 --------------------------------
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(long a = 0; a*a <= c; a++){
+            int remaining = c - (int)(a*a);
+
+            int b_start = 0, b_end = remaining;
+            while(b_start <= b_end){
+                long mid = b_start + (b_end - b_start)/2;
+
+                if(mid * mid == remaining) return true;
+                else if(mid * mid < remaining) b_start = mid + 1;
+                else b_end = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+//-----------------------------------------------------------------------------------
