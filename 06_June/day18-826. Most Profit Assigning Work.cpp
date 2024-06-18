7968d0f618a67e5f7a54fdabcb764623af79cb14
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-18 01:44:02 -0700

    June Day - 01 solved: 826. Most Profit Assigning Work (Approach -2)

--- 06_June/day18-826. Most Profit Assigning Work.cpp
+++ 06_June/day18-826. Most Profit Assigning Work.cpp
@@ -37,3 +37,51 @@ public:
 // Space Complexity -> O(n)
 
 // ----------------------------------------------------------------------------------
+
+
+// --------------------------------- Approach - 2 -----------------------------------
+
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
+        int n = difficulty.size();
+        int m = worker.size();
+        vector<pair<int, int>> vec;
+
+        for (int i = 0; i < n; i++) {
+            vec.push_back({difficulty[i], profit[i]});
+        }
+
+        sort(vec.begin(), vec.end());
+
+        for (int i = 1; i < vec.size(); i++) {
+            vec[i].second = max(vec[i].second, vec[i - 1].second);
+        }
+
+        int totalProfit = 0;
+        for (int i = 0; i < m; i++) {
+            int workerDiffLevel = worker[i];
+            int maxProfit = 0;
+            int low = 0, high = vec.size() - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (vec[mid].first <= workerDiffLevel) {
+                    maxProfit = max(maxProfit, vec[mid].second);
+                    low = mid + 1;
+                } else
+                    high = mid - 1;
+            }
+            totalProfit += maxProfit;
+        }
+
+        return totalProfit;
+    }
+};
+
+
+
+// Time Complexity -> O(nlogn)
+// Space Complexity -> O(n)
+
+// ----------------------------------------------------------------------------------
