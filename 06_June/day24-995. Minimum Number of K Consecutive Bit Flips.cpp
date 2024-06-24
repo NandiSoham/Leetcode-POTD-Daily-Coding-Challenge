80ba8b82c2d812d93e13bf1e33bb3abd3005518b
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-06-24 12:24:02 -0700

    June Day - 24 solved: 995. Minimum Number of K Consecutive Bit Flips (Approach - 2)
    constant space

--- 06_June/day24-995. Minimum Number of K Consecutive Bit Flips.cpp
+++ 06_June/day24-995. Minimum Number of K Consecutive Bit Flips.cpp
@@ -34,3 +34,37 @@ public:
 // Space Complexity -> O(n)
 
 // ----------------------------------------------------------------------------------------------
+
+// ------------------------------------------ Approach - 2 ---------------------------------------
+
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int currFlipCount = 0, totalFlips = 0;
+
+        for(int i = 0; i < n; i++){
+            if(i >= k && nums[i - k] == 5){
+                currFlipCount--;
+            }
+
+            if(currFlipCount % 2 == nums[i]){
+
+                if(i+k > n) return -1;
+
+                currFlipCount++;
+                totalFlips++;
+                nums[i] = 5;
+            }
+        }
+
+        return totalFlips;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ----------------------------------------------------------------------------------------------
