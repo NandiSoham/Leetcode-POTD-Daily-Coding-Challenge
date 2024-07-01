371d9477cf9fe83f3772624b62efa89bc45beb47
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-01 01:31:51 -0700

    July Day - 01 solved: 1550. Three Consecutive Odds (Approach -2)

--- 07_July/day01_1550. Three Consecutive Odds.cpp
+++ 07_July/day01_1550. Three Consecutive Odds.cpp
@@ -23,3 +23,25 @@ public:
 // Space Complexity ->O(1)
 
 // -----------------------------------------------------------------------------------------------
+
+// ----------------------------------------- Approach - 2 ----------------------------------------
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if (arr.size() < 3) return false;
+        for(int i = 0; i < arr.size() - 2; i++){
+            if(arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity ->O(1)
+
+// -----------------------------------------------------------------------------------------------
