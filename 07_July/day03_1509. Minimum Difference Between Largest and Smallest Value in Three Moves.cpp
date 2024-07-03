87a99c976991517df3e82532030957d06d57071e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-03 12:28:31 -0700

    July Day - 03 solved: 1509. Minimum Difference Between Largest and Smallest Value in Three Moves (Approach -2)

--- 07_July/day03_1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
+++ 07_July/day03_1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -27,3 +27,35 @@ public:
 // Space Complexity -> O(1)
 
 // ------------------------------------------------------------------------------------
+
+// ---------------------------------- Approach - 2 ------------------------------------
+
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n = nums.size();
+        int result = INT_MAX;
+        if(n <= 4)
+            return 0;
+
+        partial_sort(nums.begin(), nums.begin() + 4, nums.end());
+
+        nth_element(nums.begin() + 4, nums.begin() + (n - 4), nums.end());
+       
+        sort(nums.begin() + (n - 4), nums.end()); 
+
+        result = min(result, nums[n-4] - nums[0]);
+        result = min(result, nums[n-1] - nums[3]);
+        result = min(result, nums[n-3] - nums[1]);
+        result = min(result, nums[n-2] - nums[2]);
+
+        return result;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ------------------------------------------------------------------------------------
