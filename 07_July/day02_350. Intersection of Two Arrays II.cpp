dc3b8c8974ce9102fbdee124eb35c7e4217252e2
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-02 22:35:00 -0700

    July Day - 02 solved: 350. Intersection of Two Arrays II (Approach-2)

--- 07_July/day02_350. Intersection of Two Arrays II.cpp
+++ 07_July/day02_350. Intersection of Two Arrays II.cpp
@@ -1,5 +1,7 @@
 // Problem Link -> https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
 
+// ---------------------------------- Approach - 1 -------------------------------
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -25,3 +27,36 @@ public:
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(n)
+
+// -------------------------------------------------------------------------------
+
+// ---------------------------------- Approach - 2 -------------------------------
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int ptr1 = 0, ptr2 = 0;
+        vector<int> ans;
+
+        while(ptr1 < nums1.size() && ptr2 < nums2.size()){
+            if(nums1[ptr1] == nums2[ptr2]){
+                ans.push_back(nums1[ptr1]);
+                ptr1++;
+                ptr2++;
+            } else if(nums1[ptr1] < nums2[ptr2]){
+                ptr1++;
+            } else ptr2++;
+        }
+
+        return ans;
+    }
+};
+
+
+
+// Time Complexity -> O(nlogn)
+// Space Complexity -> O(1)
+
+// -------------------------------------------------------------------------------
