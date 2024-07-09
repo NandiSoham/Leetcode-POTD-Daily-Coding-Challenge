98fd6acb5380c3a75af74b802356c80d8dd9abce
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-08 22:23:08 -0700

    July Day - 08 solved: 1823. Find the Winner of the Circular Game (Approach - 2)

--- 07_July/day08_1823. Find the Winner of the Circular Game.cpp
+++ 07_July/day08_1823. Find the Winner of the Circular Game.cpp
@@ -27,3 +27,36 @@ public:
 // Space Complexity -> O(1)
 
 // ---------------------------------------------------------------------------------------
+
+// ------------------------------------ Approach - 2 -------------------------------------
+
+class Solution {
+public:
+
+    int findWinnerIdx(int n, int k) {
+        if(n == 1) {
+            return 0; //index
+        }
+
+        int idx = findWinnerIdx(n-1, k);
+        idx = (idx + k) % n; //to find the original index in the original array
+
+        return idx;
+    }
+
+    int findTheWinner(int n, int k) {
+        
+        int result_idx = findWinnerIdx(n, k);
+
+
+        return result_idx + 1;
+
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ---------------------------------------------------------------------------------------
