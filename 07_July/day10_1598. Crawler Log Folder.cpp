5d36c669e0d0fd2260c7be8075887fbb59c6100b
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-10 00:36:22 -0700

    July Day - 10 solved: 1598. Crawler Log Folder (Approach - 2)

--- 07_July/day10_1598. Crawler Log Folder.cpp
+++ 07_July/day10_1598. Crawler Log Folder.cpp
@@ -28,3 +28,33 @@ public:
 // Space Complexity O(n)
 
 // ---------------------------------------------------------------------------
+
+// ------------------------------- Approach - 1 ------------------------------
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+
+        for (string& log : logs) {
+            if (log == "../") {
+                depth--;
+                if (depth < 0) {
+                    depth = 0;
+                }
+            } else if (log == "./")
+                continue;
+            else
+                depth++;
+        }
+
+        return depth;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity O(1)
+
+// ---------------------------------------------------------------------------
