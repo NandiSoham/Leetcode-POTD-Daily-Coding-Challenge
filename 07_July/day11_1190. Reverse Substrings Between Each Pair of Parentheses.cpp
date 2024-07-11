91aa79bc4b2e4362c4327d0d575cfc3d41958262
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-07-11 01:03:29 -0700

    July Day - 11 solved: 1190. Reverse Substrings Between Each Pair of Parentheses (Approach - 2)

--- 07_July/day11_1190. Reverse Substrings Between Each Pair of Parentheses.cpp
+++ 07_July/day11_1190. Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -29,3 +29,44 @@ public:
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(n)
 // --------------------------------------------------------------------------------
+
+// ---------------------------------- Approach - 2 --------------------------------
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n = s.length();
+        stack<int> openIdx;
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                openIdx.push(i);
+            } else if(s[i] == ')'){
+                int j = openIdx.top();
+                openIdx.pop();
+                arr[i] = j;
+                arr[j] = i;
+            }
+        }
+
+        string ans = "";
+        int flag = 1;
+        for(int i = 0; i < n; i += flag){
+            if(s[i] == '(' || s[i] == ')'){
+                i = arr[i];
+                flag = -flag;
+            } else {
+                ans.push_back(s[i]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+// --------------------------------------------------------------------------------
