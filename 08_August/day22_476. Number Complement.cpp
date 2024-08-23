0b0f9325bee354157af03bc9d798aa51c571293e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-08-22 19:13:16 -0700

    August Day - 22 solved: 476. Number Complement (Approach-3)

--- 08_August/day22_476. Number Complement.cpp
+++ 08_August/day22_476. Number Complement.cpp
@@ -42,3 +42,24 @@ public:
 // Space Complexity -> O(1)
 
 // -----------------------------------------------------------------------------------------
+
+
+// ------------------------------------- Approach - 2 --------------------------------------
+
+class Solution {
+public:
+    int findComplement(int num) {
+        if (num == 0) return 1;
+
+        int number_of_bits = (int)(log2(num)) + 1;
+        unsigned int mask = (1U << number_of_bits) - 1; 
+
+        return num ^ mask;
+    }
+};
+
+
+// Time Complexity -> O(log2(num))
+// Space Complexity -> O(1)
+
+// -----------------------------------------------------------------------------------------
