8b4f854e3b03e609173f7ecd9a7fa5117b72457d
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-08-25 12:19:37 -0700

    August Day - 22 solved: 145. Binary Tree Postorder Traversal

---
+++ 08_August/day25_145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,19 @@
+// Problem Link -> https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+class Solution {
+public:
+
+    void postOrder(TreeNode* root, vector<int> &ans){
+        if(root == NULL) return;
+
+        postOrder(root->left, ans);
+        postOrder(root->right, ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postOrder(root, ans);
+        return ans;
+    }
+};
