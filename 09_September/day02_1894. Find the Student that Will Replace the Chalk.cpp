ffaa7fea7c3c3cfad04daaa932ebb321af9e0c78
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-09-02 15:16:36 -0700

    September Day - 02 solved: 1894. Find the Student that Will Replace the Chalk (Approach-2)

--- 09_September/day02_1894. Find the Student that Will Replace the Chalk.cpp
+++ 09_September/day02_1894. Find the Student that Will Replace the Chalk.cpp
@@ -1,6 +1,6 @@
 // Problem Link -> https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/description/
 
-// ------------------------- Approach ( TLE issue ) ----------------------
+// ------------------------- Approach - 1 ( TLE issue ) ----------------------
 
 class Solution {
 public:
@@ -16,5 +16,35 @@ public:
         return -1;
     }
 };
+
+// -----------------------------------------------------------------------
+
+
+// -------------------------- Approach - 2 -------------------------------
+
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n = chalk.size();
+        long long chalkSum = 0;
+
+        for(int &chalkNeeded : chalk){
+            chalkSum += chalkNeeded;
+        }
+
+        int remainingChalk = k % chalkSum;
+        for(int i = 0; i < n; i++){
+            if(remainingChalk < chalk[i]) return i;
+            remainingChalk -= chalk[i];
+        }
+
+        return -1;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Soace Complexity -> O(1)
 
 // -----------------------------------------------------------------------
