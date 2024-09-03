c6993979d6dd67e79ea485bbe95e4e503e603cb1
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-09-03 01:18:54 -0700

    September Day - 03 solved: 1945. Sum of Digits of String After Convert (Approach-2)

--- 09_September/day03_1945. Sum of Digits of String After Convert.cpp
+++ 09_September/day03_1945. Sum of Digits of String After Convert.cpp
@@ -34,3 +34,37 @@ public:
 // Space Complexity -> O(1)
 
 // -------------------------------------------------------------------------
+
+
+// ------------------------------ Approach - 2 -----------------------------
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string numericString = "";
+
+        for(auto ch: s){
+            int charNumericVal = ch - 'a' + 1;
+            numericString += to_string(charNumericVal);
+        }
+
+        int digitSum = 0;
+        while(k--){
+            digitSum = 0;
+            for(char ch : numericString){
+                digitSum += ch - '0';
+            }
+
+            numericString = to_string(digitSum);
+        }
+
+        return stoi(numericString);
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+// -------------------------------------------------------------------------
