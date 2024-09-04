29fc3d7c07864ae96e80f0dac764199479e2ec6f
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-09-04 16:24:54 -0700

    September Day - 04 solved: 874. Walking Robot Simulation

---
+++ 09_September/day04_874. Walking Robot Simulation.cpp
@@ -0,0 +1,39 @@
+// Problem Link -> https://leetcode.com/problems/walking-robot-simulation/description/
+
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string> obstacleSet;
+        for(auto &it : obstacles){
+            string obstacleKey = to_string(it[0]) + '-' + to_string(it[1]);
+            obstacleSet.insert(obstacleKey);
+        }
+
+        int currentX = 0, currentY = 0;
+        int maxDistance = 0;
+        pair<int, int> direction = {0, 1};
+
+        for(int i = 0; i < commands.size(); i++){
+            if(commands[i] == -1){
+                direction = {direction.second, -direction.first};
+            } else if (commands[i] == -2){
+                direction = {-direction.second, direction.first};
+            } else {
+                for(int step = 0; step < commands[i]; step++){
+                    int nextX = currentX + direction.first;
+                    int nextY = currentY + direction.second;
+                    string newPositionKey = to_string(nextX) + '-' + to_string(nextY);
+
+                    if(obstacleSet.find(newPositionKey) != obstacleSet.end()) break;
+
+                    currentX = nextX;
+                    currentY = nextY;
+                }
+            }
+
+            maxDistance = max(maxDistance, currentX * currentX + currentY * currentY);
+        }
+
+        return maxDistance;
+    }
+};
