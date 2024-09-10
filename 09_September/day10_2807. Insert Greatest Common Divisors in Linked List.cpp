a0d973b7c214c4fbcf415268fc33c3e462d94eef
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-09-10 00:40:41 -0700

    September Day - 10 solved: 2807. Insert Greatest Common Divisors in Linked List ( Approach - 1 )

---
+++ 09_September/day10_2807. Insert Greatest Common Divisors in Linked List.cpp
@@ -0,0 +1,33 @@
+// Problem Link -> https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/
+
+// ------------------------------------ Approach -1 ----------------------------------
+
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return head;
+        }
+
+        ListNode* currNode = head;
+        ListNode* nextNode = head -> next;
+
+        while(nextNode != NULL){
+            ListNode* gcdNode = new ListNode(__gcd(currNode -> val, nextNode -> val));
+            currNode -> next = gcdNode;
+            gcdNode -> next = nextNode;
+
+            currNode = nextNode;
+            nextNode = nextNode -> next;
+        }
+
+        return head;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Compelxity -> O(1)
+
+// ------------------------------------------------------------------------------------
