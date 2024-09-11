3980eb52e805ec02caf7fea9e6d2c976cf82b412
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-09-11 00:39:20 -0700

    September Day - 11 solved: 2220. Minimum Bit Flips to Convert Number ( Approach - 1 )

---
+++ 09_September/day11_2220. Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,26 @@
+// Problem link -> https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
+
+// -------------------------------- Approach - 1 ---------------------------------
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+
+        while(start > 0 || goal > 0) {
+            if((start & 1) != (goal & 1)) {
+                count++;
+            }
+            start = (start >> 1);
+            goal  = (goal >> 1);
+        }
+
+        return count;
+    }
+};
+
+
+// Time Complexity -> O(1)
+// Space Complexity -> O(1)
+
+// ---------------------------------------------------------------------------------
