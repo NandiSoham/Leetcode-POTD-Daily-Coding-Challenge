88741c5d490eae78a5153184331dc744b9c894b4
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-10-23 22:36:11 +0530

    October Day - 23 solved: 2641. Cousins in Binary Tree II

---
+++ 10_October/day23_2641. Cousins in Binary Tree II.cpp
@@ -0,0 +1,48 @@
+// Problem Link -> https://leetcode.com/problems/cousins-in-binary-tree-ii/description/
+
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        if (root == NULL) {
+            return root;
+        }        
+
+        queue<TreeNode*> que;
+        que.push(root);
+        int levelSum = root->val;
+
+        while (!que.empty()) {
+            int n = que.size();
+            int nextLevelSum = 0;
+
+            while (n--) {
+                TreeNode* curr = que.front();
+                que.pop();
+                curr->val = levelSum - curr->val;
+
+                int siblingSum = (curr->left != NULL ? curr->left->val : 0);
+                siblingSum += (curr->right != NULL ? curr->right->val : 0);
+
+                if (curr->left) {
+                    nextLevelSum += curr->left->val;
+                    curr->left->val = siblingSum;  
+                    que.push(curr->left);
+                }
+
+                if (curr->right) {
+                    nextLevelSum += curr->right->val;
+                    curr->right->val = siblingSum; 
+                    que.push(curr->right);
+                }
+            }
+            levelSum = nextLevelSum;
+        }
+
+        return root;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
