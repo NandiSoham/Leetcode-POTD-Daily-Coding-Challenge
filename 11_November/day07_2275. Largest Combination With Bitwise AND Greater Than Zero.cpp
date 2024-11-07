33a7625738569c0ad81d0068f6d5c9354affa5db
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-11-07 23:40:50 +0530

    November Day - 07 solved: 2275. Largest Combination With Bitwise AND Greater Than Zero

---
+++ 11_November/day07_2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -0,0 +1,25 @@
+// Problem Link -> https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/description/
+
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int result = 0;
+
+        for(int i = 0; i < 24; i++) {
+            int count_ith = 0;
+
+            for(int &num : candidates) {
+                if((num & (1 << i)) != 0) {
+                    count_ith += 1;
+                }
+            }
+
+            result = max(result, count_ith);
+        }
+
+        return result;
+    }
+};
+
+
+
