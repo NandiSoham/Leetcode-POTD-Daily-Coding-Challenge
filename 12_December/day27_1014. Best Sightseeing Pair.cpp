c54d763a509613de50c237b34812be1eb5c2211e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-12-27 14:01:06 -0800

    December Day - 27 solved: 1014. Best Sightseeing Pair

---
+++ 12_December/day27_1014. Best Sightseeing Pair.cpp
@@ -0,0 +1,27 @@
+// Problem Link -> https://leetcode.com/problems/best-sightseeing-pair/description/
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int n = values.size();
+
+        int result = 0;
+
+        int max_till_now = values[0] + 0;
+
+        for (int j = 1; j < n; j++) {
+            int x = values[j] - j;
+            int y = max_till_now;
+
+            result = max(result, x + y);
+            max_till_now = max(max_till_now, values[j] + j);
+        }
+
+        return result;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
