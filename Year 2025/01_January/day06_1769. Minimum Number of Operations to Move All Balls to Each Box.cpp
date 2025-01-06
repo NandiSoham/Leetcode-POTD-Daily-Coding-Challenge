b3b7ee92f21678bdcb8bf0e50b70fbf6bda58e5e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-06 12:50:08 -0800

    January Day - 06 solved: 1769. Minimum Number of Operations to Move All Balls to Each Box (Approach - 2)

--- Year 2025/01_January/day06_1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ Year 2025/01_January/day06_1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -33,3 +33,33 @@ public:
 //Space Complexity: O (1)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 1 ====================================
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.length();
+
+        vector<int> answer(n, 0);
+
+        for(int i = 0; i < n; i++) {
+
+            if(boxes[i] == '1') {
+                for(int j = 0; j < n; j++) {
+                    answer[j] += abs(j-i);
+                }
+            }
+        }
+
+        return answer;
+    }
+};
+
+
+
+// Time Complexity: O (n^2)
+//Space Complexity: O (1)
+
+// =========================================================================================
