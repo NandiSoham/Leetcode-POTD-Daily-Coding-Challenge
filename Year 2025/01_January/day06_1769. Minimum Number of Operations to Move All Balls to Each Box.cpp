b52049408c7ba27ed346fa0959246ae237ba4599
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-06 12:49:11 -0800

    January Day - 06 solved: 1769. Minimum Number of Operations to Move All Balls to Each Box (Approach - 1)

---
+++ Year 2025/01_January/day06_1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -0,0 +1,35 @@
+// Problem Link -> https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+
+// ======================================= Approach - 1 ====================================
+
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.length();
+        unordered_set<int> st;
+
+        for(int i = 0; i < n; i++) {
+            if(boxes[i] == '1') {
+                st.insert(i);
+            }
+        }
+
+        vector<int> answer(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            for(auto &idx : st) {
+                answer[i] += abs(i - idx);
+            }
+        }
+
+        return answer;
+
+    }
+};
+
+
+
+// Time Complexity: O (n^2)
+//Space Complexity: O (1)
+
+// =========================================================================================
