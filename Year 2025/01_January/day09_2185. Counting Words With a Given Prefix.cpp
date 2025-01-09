249cd71e82c7d6b6005f8ec7bf1989e2b0470eed
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-09 09:56:02 -0800

    January Day - 09 solved: 2185. Counting Words With a Given Prefix (Approach - 2)

--- Year 2025/01_January/day09_2185. Counting Words With a Given Prefix.cpp
+++ Year 2025/01_January/day09_2185. Counting Words With a Given Prefix.cpp
@@ -21,3 +21,74 @@ public:
 // Space Complexity: O (n ^ m)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 1 ====================================
+
+struct TrieNode {
+    TrieNode* children[26];
+    bool isEndOfWord;
+    int prefixCount;
+
+    TrieNode() {
+        isEndOfWord = false;
+        prefixCount = 0;
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string& word) {
+        TrieNode* currentNode = root;
+        for (char& ch : word) {
+            int index = ch - 'a';
+            if (currentNode->children[index] == nullptr) {
+                currentNode->children[index] = new TrieNode();
+            }
+            currentNode = currentNode->children[index];
+            currentNode->prefixCount++;
+        }
+        currentNode->isEndOfWord = true;
+    }
+
+    int searchPrefixCount(string& prefix) {
+        TrieNode* currentNode = root;
+        for (char& ch : prefix) {
+            int index = ch - 'a';
+            if (currentNode->children[index] == nullptr) {
+                return 0;
+            }
+            currentNode = currentNode->children[index];
+        }
+        return currentNode->prefixCount;
+    }
+};
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        Trie trie;
+
+        for (string& word : words) {
+            trie.insert(word);
+        }
+
+        return trie.searchPrefixCount(pref);
+    }
+};
+
+
+
+// Time Complexity: O (n ^ l + m)
+// Space Complexity: O (n ^ l)
+
+// =========================================================================================
