9dd55ae54740fe437371cddf3c4cbd0674e96190
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-15 11:06:04 -0800

    January Day - 15 solved: 2429. Minimize XOR (Approach - 2)

--- Year 2025/01_January/day15_2429. Minimize XOR.cpp
+++ Year 2025/01_January/day15_2429. Minimize XOR.cpp
@@ -50,3 +50,54 @@ public:
 // Space Complexity -> O(1)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    bool isBitSet(int &number, int position) {
+        return number & (1 << position);
+    }
+
+    bool enableBit(int &number, int position) {
+        return number |= (1 << position);
+    }
+
+    bool disableBit(int &number, int position) {
+        return number &= ~(1 << position);
+    }
+
+    bool isBitUnset(int number, int position) {
+        return (number & (1 << position)) == 0;
+    }
+
+    int minimizeXor(int number1, int number2) {
+        int result = 0;
+
+        int targetSetBits = __builtin_popcount(number2);
+
+        for (int position = 31; position >= 0 && targetSetBits > 0; position--) {
+            if (isBitSet(number1, position)) {
+                enableBit(result, position);
+                targetSetBits--;
+            }
+        }
+
+        for (int position = 0; position < 32 && targetSetBits > 0; position++) {
+            if (isBitUnset(number1, position)) {
+                enableBit(result, position);
+                targetSetBits--;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+// Time Complexity -> O(log(n))
+// Space Complexity -> O(1)
+
+// =========================================================================================
