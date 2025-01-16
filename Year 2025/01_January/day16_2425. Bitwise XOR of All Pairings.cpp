7ed00e9c64ab89ceb23dd9e92ed77926b34c6521
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-16 12:08:08 -0800

    January Day - 16 solved: 2425. Bitwise XOR of All Pairings (Approach - 2)

--- Year 2025/01_January/day16_2425. Bitwise XOR of All Pairings.cpp
+++ Year 2025/01_January/day16_2425. Bitwise XOR of All Pairings.cpp
@@ -37,3 +37,36 @@ public:
 // Space Complexity -> O(n + m)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int XOR_result = 0;
+
+        if(n % 2 != 0){
+            for(int& num : nums2){
+                XOR_result ^= num;
+            }
+        }
+
+        if(m % 2 != 0){
+            for(int& num : nums1){
+                XOR_result ^= num;
+            }
+        }
+
+        return XOR_result;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// =========================================================================================
