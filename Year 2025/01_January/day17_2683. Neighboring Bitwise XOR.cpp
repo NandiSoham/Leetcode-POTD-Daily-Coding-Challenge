47daafef026b942ead0ad33a6055c16efc2b1b43
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-17 10:33:29 -0800

    January Day - 17 solved: 2683. Neighboring Bitwise XOR (Approach - 1)

---
+++ Year 2025/01_January/day17_2683. Neighboring Bitwise XOR.cpp
@@ -0,0 +1,37 @@
+// Problem link -> https://leetcode.com/problems/neighboring-bitwise-xor/description/
+
+// ======================================= Approach - 1 ====================================
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n = derived.size();
+        vector<int> originalArr(n);
+
+        originalArr[0] = 0;
+        for(int i = 0; i < n - 1; i++){
+            originalArr[i + 1] = originalArr[i] ^ derived[i];
+        }
+        if((originalArr[n - 1] ^ originalArr[0]) == derived[n - 1]){
+            return true;
+        }
+
+
+        originalArr[0] = 1;
+        for(int i = 0; i < n - 1; i++){
+            originalArr[i + 1] = originalArr[i] ^ derived[i];
+        }
+        if((originalArr[n - 1] ^ originalArr[0]) == derived[n - 1]){
+            return true;
+        }
+
+        return false;s
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+// =========================================================================================
