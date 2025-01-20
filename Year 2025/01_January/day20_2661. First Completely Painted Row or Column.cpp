d88e93fb8bdaf214f3b27b164e4b9f6bbd93836f
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-20 14:32:44 -0800

    January Day - 20 solved: 2661. First Completely Painted Row or Column (Approach - 1)

---
+++ Year 2025/01_January/day20_2661. First Completely Painted Row or Column.cpp
@@ -0,0 +1,43 @@
+// Problem Link -> https://leetcode.com/problems/first-completely-painted-row-or-column/description/
+
+// ======================================= Approach - 1 ====================================
+
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int r = mat.size();
+        int c = mat[0].size();
+        unordered_map<int, pair<int, int>> coordinateMap;
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                int val = mat[i][j];
+                coordinateMap[val] = {i, j};
+            }
+        }
+
+        vector<int> paintedRowCount(r, 0);
+        vector<int> paintedColCount (c, 0);
+
+        for(int i = 0; i < arr.size(); i++){
+            int val = arr[i];
+            auto [row, col] = coordinateMap[val];
+
+            paintedRowCount[row]++;
+            paintedColCount[col]++;
+
+            if(paintedRowCount[row] == c || paintedColCount[col] == r){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+
+// Time Complexity: O (r * c)
+// Space Complexity: O((r*c) + r + c)
+
+// =========================================================================================
