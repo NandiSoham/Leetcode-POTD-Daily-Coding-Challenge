68760c010b5c457ea5217902dd2038afc78a2a67
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-23 14:32:08 -0800

    January Day - 23 solved: 1267. Count Servers that Communicate (Approach - 2)

--- Year 2025/01_January/day23_1267. Count Servers that Communicate.cpp
+++ Year 2025/01_January/day23_1267. Count Servers that Communicate.cpp
@@ -45,6 +45,46 @@ public:
 
 
 
+// Time Complexity: O((m*n) * (m+n))
+// Space Complexity: O(1)
+
+// =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> indexColCount(n, 0);
+        vector<int> indexRowCount(m, 0);
+    
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(grid[row][col] == 1) { 
+                    indexColCount[col] += 1;
+                    indexRowCount[row] += 1;
+                }
+            }
+        }
+
+        int resultServers = 0;
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(grid[row][col] == 1 && (indexColCount[col] > 1 || indexRowCount[row] > 1)) {
+                    resultServers++;
+                }
+            }
+        }
+        return resultServers;
+    }
+};
+
+
+
 // Time Complexity: O (m * n)
 // Space Complexity: O (m + n)
 
