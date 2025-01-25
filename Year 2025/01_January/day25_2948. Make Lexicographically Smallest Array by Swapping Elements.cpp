9fb412ad4d4c6d77b093854890f015f57fd81b10
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-01-25 12:46:51 -0800

    January Day - 23 solved: 2948. Make Lexicographically Smallest Array by Swapping Elements (Approach - 2)

--- Year 2025/01_January/day25_2948. Make Lexicographically Smallest Array by Swapping Elements.cpp
+++ Year 2025/01_January/day25_2948. Make Lexicographically Smallest Array by Swapping Elements.cpp
@@ -36,3 +36,46 @@ public:
 // Space Complexity -> O(1)
 
 // =========================================================================================
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<int> sortedNums = nums;
+        sort(sortedNums.begin(), sortedNums.end());
+
+        int currentGroup = 0;
+        unordered_map<int, int> numToGroup;
+        numToGroup[sortedNums[0]] = currentGroup;
+
+        unordered_map<int, list<int>> groupToElements;
+        groupToElements[currentGroup].push_back(sortedNums[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (abs(sortedNums[i] - sortedNums[i - 1]) > limit) {
+                currentGroup++;
+            }
+
+            numToGroup[sortedNums[i]] = currentGroup;
+            groupToElements[currentGroup].push_back(sortedNums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int group = numToGroup[num];
+            nums[i] = *groupToElements[group].begin();
+            groupToElements[group].pop_front();
+        }
+
+        return nums;
+    }
+};
+
+
+
+// Time Complexity -> O(n*logn)
+// Space Complexity -> O(n)
+
+// =========================================================================================
