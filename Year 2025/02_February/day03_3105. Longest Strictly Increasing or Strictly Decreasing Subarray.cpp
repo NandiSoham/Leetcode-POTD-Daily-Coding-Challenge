1fc96f00d705f86fb5979864f014cec5166f8371
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-02 21:07:42 -0800

    February Day - 03 solved: 3105. Longest Strictly Increasing or Strictly Decreasing Subarray (Approach - 2)

--- Year 2025/02_February/day03_3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
+++ Year 2025/02_February/day03_3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -38,4 +38,36 @@ public:
 // Time Complexity: O (n ^ 2)
 // Space Complexity: O (1)
 
-// ===============================================================================================
+// =========================================================================================
+
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int maxLength = 1;
+        int strictlyIncreasingLen = 1;
+        int strictlyDecreasingLen = 1;
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > nums[i - 1]) strictlyIncreasingLen++;
+            else strictlyIncreasingLen = 1;
+
+            if(nums[i] < nums[i - 1])strictlyDecreasingLen++;
+            else strictlyDecreasingLen = 1;
+
+            maxLength = max({maxLength, strictlyIncreasingLen, strictlyDecreasingLen});
+        }
+
+        return maxLength;
+    }
+};
+
+
+
+// Time Complexity: O (n)
+// Space Complexity: O (1)
+
+// =========================================================================================
