4af1b9c536c2f2823577e23e24470d5ac177a446
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-03 20:15:16 -0800

    February Day - 04 solved: 1800. Maximum Ascending Subarray Sum (Approach - 2)

--- Year 2025/02_February/day04_1800. Maximum Ascending Subarray Sum.cpp
+++ Year 2025/02_February/day04_1800. Maximum Ascending Subarray Sum.cpp
@@ -33,3 +33,34 @@ public:
 // Space Complexity -> O(1)
 
 // =========================================================================================
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxSum = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            int curSum = nums[i];
+            int j = i + 1;
+
+            while(j < n && nums[j] > nums[j - 1]){
+                curSum += nums[j];
+                j++;
+            }
+
+            maxSum = max(curSum, maxSum);
+        }
+
+        return maxSum;
+    }
+};
+
+
+
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(1)
+
+// =========================================================================================
