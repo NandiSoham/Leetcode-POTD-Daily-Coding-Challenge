ab547f0bb94242beca89990c0d34428581f1ed44
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-05 23:27:41 -0800

    February Day - 0 solved: 1726. Tuple with Same Product (Approach - 1)

--- Year 2025/02_February/day06_1726. Tuple with Same Product.cpp
+++ Year 2025/02_February/day06_1726. Tuple with Same Product.cpp
@@ -36,3 +36,37 @@ public:
 // Space Complexity -> O(n)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size();
+        int tuplesCount = 0;
+        unordered_map<int, int> productFrequency;
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                int prodElement = nums[i] * nums[j]; 
+                productFrequency[prodElement]++;
+
+            }
+        }
+
+        int totalCount = 0;
+        for (auto [prodElement, freq] : productFrequency) {
+            tuplesCount += 8 * (freq * (freq - 1))/2;        // nC2 -> (n * (n-1))/2
+        }
+
+        return tuplesCount;
+    }
+};
+
+
+
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(n)
+
+// =========================================================================================
