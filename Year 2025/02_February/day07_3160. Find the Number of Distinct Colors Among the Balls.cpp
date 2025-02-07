19fbc2882bd9a70820d775c9b45e8e6582059f44
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-06 20:26:53 -0800

    February Day - 07 solved: 3160. Find the Number of Distinct Colors Among the Balls (Approach - 1)

---
+++ Year 2025/02_February/day07_3160. Find the Number of Distinct Colors Among the Balls.cpp
@@ -0,0 +1,41 @@
+// Problem Link -> https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls/
+
+// ======================================= Approach - 1 (MLE) ====================================
+
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        int n = queries.size();
+        vector<int> ansArr(n);
+        vector<int> ballColor(limit+1, -1);
+        unordered_map<int, int> colorFreqMap;
+
+        for(int i = 0; i < n; i++){
+            int currBall = queries[i][0];
+            int currColor = queries[i][1];
+
+            if(ballColor[currBall] != -1){ // this means ball is already colored
+                int prevColor = ballColor[currBall];
+                colorFreqMap[prevColor]--;
+
+                if(colorFreqMap[prevColor] == 0){
+                    colorFreqMap.erase(prevColor);
+                }
+            }
+
+            ballColor[currBall] = currColor;   // when ball is not colored
+            colorFreqMap[currColor]++;
+            
+            ansArr[i] = colorFreqMap.size(); 
+        }
+
+        return ansArr;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(limit)
+
+// =========================================================================================
