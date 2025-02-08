50b5eb613f9044580b95bcff7bd1cbd882cbaeb5
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-08 12:46:54 -0800

    February Day - 08 solved: 2349. Design a Number Container System (Approach - 2)

--- Year 2025/02_February/day08_2349. Design a Number Container System.cpp
+++ Year 2025/02_February/day08_2349. Design a Number Container System.cpp
@@ -40,3 +40,39 @@ public:
 //Space Complexity -> O(n)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class NumberContainers {
+public:
+    unordered_map<int, int> idxToNum;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> numToIdx;
+
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        idxToNum[index] = number;
+        numToIdx[number].push(index);
+    }
+    
+    int find(int number) {
+        if(numToIdx.count(number)){
+            auto& queueItem = numToIdx[number];
+
+            while(!queueItem.empty()){
+                int idx = queueItem.top();
+
+                if(idxToNum[idx] == number) return idx;
+
+                queueItem.pop();
+            }
+        }
+
+        return -1;
+    }
+};
+
+// =========================================================================================
