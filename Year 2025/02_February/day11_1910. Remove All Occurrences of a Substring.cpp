4c7a787b282fc222291b0f1860a57fbacdb5ded5
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-10 23:31:49 -0800

    February Day - 11 solved: 1910. Remove All Occurrences of a Substring (Approach-2)

--- Year 2025/02_February/day11_1910. Remove All Occurrences of a Substring.cpp
+++ Year 2025/02_February/day11_1910. Remove All Occurrences of a Substring.cpp
@@ -19,3 +19,54 @@ public:
 // Space COmplexity -> O(1)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+
+    bool checkFunc(stack<char>& charSt, string& part, int n){
+        stack<char> tempSt = charSt;
+
+        for(int i = n - 1; i >= 0; i--){
+            if(tempSt.top() != part[i]){
+                return false;
+            }
+            tempSt.pop();
+        }
+        return true;
+    }
+
+    string removeOccurrences(string s, string part) {
+        stack<char> charSt;
+        int m = s.length();
+        int n = part.length();
+        string result = "";
+
+        for(int i = 0; i < m; i++){
+            charSt.push(s[i]);
+
+            if(charSt.size() >= n && checkFunc(charSt, part, n) == true){
+                for(int j = 0; j < n; j++){
+                    charSt.pop();
+                }
+            }
+        }
+
+        while(!charSt.empty()){
+            result.push_back(charSt.top());
+            charSt.pop();
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+
+
+// Time Complexity -> O(m*n)
+// Space COmplexity -> O(m)
+
+// =========================================================================================
