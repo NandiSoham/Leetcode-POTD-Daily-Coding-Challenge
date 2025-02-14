0d712058338417bf1de97a3c7a236db0333ef31e
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-13 23:16:19 -0800

    February Day - 14 solved: 1352. Product of the Last K Numbers (Approach - 2)

--- Year 2025/02_February/day14_1352. Product of the Last K Numbers.cpp
+++ Year 2025/02_February/day14_1352. Product of the Last K Numbers.cpp
@@ -34,3 +34,48 @@ public:
 // Space Complexity -> O(n)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class ProductOfNumbers {
+public:
+    vector<int> cumulativeProduct;
+    int n;
+
+    ProductOfNumbers() {
+        cumulativeProduct.clear();
+        n = 0;
+    }
+    
+    void add(int num) {
+        if (num == 0) {
+            cumulativeProduct.clear();
+            n = 0;
+        } else {
+            if (cumulativeProduct.empty()) {
+                cumulativeProduct.push_back(num);
+            } else {
+                cumulativeProduct.push_back(cumulativeProduct[n - 1] * num);
+            }
+            n++;
+        }
+    }
+    
+    int getProduct(int k) {
+        if (k > n) {
+            return 0;
+        }
+        if (k == n) {
+            return cumulativeProduct[n - 1];
+        }
+        return cumulativeProduct[n - 1] / cumulativeProduct[n - k - 1];
+    }
+};
+
+
+
+// Time Complexity -> O(1)
+// Space Complexity -> O(n)
+
+// =========================================================================================
