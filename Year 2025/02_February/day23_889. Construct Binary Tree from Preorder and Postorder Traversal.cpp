e2295df04f928716a245a3ff73a8732243097d3d
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-23 12:08:03 -0800

    February Day - 23 solved: 889. Construct Binary Tree from Preorder and Postorder Traversal (Approach -2)

--- Year 2025/02_February/day23_889. Construct Binary Tree from Preorder and Postorder Traversal.cpp
+++ Year 2025/02_February/day23_889. Construct Binary Tree from Preorder and Postorder Traversal.cpp
@@ -35,4 +35,43 @@ public:
 // Time Complexity ->O(n^2)
 // Space Complexity -> O(n)
 
-// ===============================================================================================
+// =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    TreeNode* buildTree(int preStart, int postStart, int preEnd, vector<int>& preorder, vector<int>& postorder, unordered_map<int, int>& postorderNodeMap;){
+        if (preStart > preEnd) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        if (preStart == preEnd) return root;
+
+        int leftSubtreeRoot = preorder[preStart + 1];
+        int j = postorderNodeMap[leftSubtreeRoot];
+
+        int nodeCount = j - postStart + 1;
+
+        root -> left = buildTree(preStart + 1, postStart, preStart + nodeCount, preorder, postorder);
+        root -> right = buildTree (preStart + nodeCount + 1, j + 1, preEnd, preorder, postorder);
+
+        return root;
+    }
+
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        unordered_map<int, int> postorderNodeMap;
+        for(int i = 0; i < postorder.size(); i++){
+            postorderNodeMap[postorder[i]] = i;
+        }
+
+        return buildTree(0, 0, preorder.size() - 1, preorder, postorder, postorderNodeMap);
+    }
+};
+
+
+
+// Time Complexity ->O(n)
+// Space Complexity -> O(n)
+
+// =========================================================================================
