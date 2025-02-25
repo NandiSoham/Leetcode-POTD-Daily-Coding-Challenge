87af644f986a824a0d85c58f0c0a86f20ff3efc4
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-25 00:05:16 -0800

    February Day - 25 solved: 1524. Number of Sub-arrays With Odd Sum (Approach -2)

--- Year 2025/02_February/day25_1524. Number of Sub-arrays With Odd Sum.cpp
+++ Year 2025/02_February/day25_1524. Number of Sub-arrays With Odd Sum.cpp
@@ -30,3 +30,33 @@ public:
 // Space Complexity -> O(1)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 (better version of Approach -1 ) ====================================
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int mod = 1e9 + 7;;
+        int count = 0;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++){
+            int currSum = 0;
+
+            for (int j = i; j < n; j++){
+                currSum += arr[j];
+                
+                if(currSum % 2 != 0) count++;
+            }
+        }
+
+        return count % mod;
+    }
+};
+
+
+// Time Complexity ->O(n^2)
+// Space Complexity -> O(1)
+
+// =========================================================================================
