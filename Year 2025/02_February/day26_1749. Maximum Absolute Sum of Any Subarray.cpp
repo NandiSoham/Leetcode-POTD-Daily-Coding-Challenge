658351365571e14f6a30d1655d621c6c686f4a28
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-02-26 00:22:41 -0800

    February Day - 26 solved: 1749. Maximum Absolute Sum of Any Subarray (Approach -2)

--- Year 2025/02_February/day26_1749. Maximum Absolute Sum of Any Subarray.cpp
+++ Year 2025/02_February/day26_1749. Maximum Absolute Sum of Any Subarray.cpp
@@ -28,6 +28,38 @@ public:
 
 
 // Time Complexity ->O(n)
-// Space Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        int currMaxSum = nums[0];
+        int currMinSum = nums[0];
+
+        int maxSubArrSum = nums[0];
+        int minSubArrSum = nums[0];
+
+        for(int i = 1; i < n; i++){
+            currMaxSum = max (nums[i], currMaxSum + nums[i]);
+            maxSubArrSum = max (maxSubArrSum, currMaxSum);
+
+            currMinSum = min (nums[i], currMinSum + nums[i]);
+            minSubArrSum = min (minSubArrSum, currMinSum);
+        }
+
+        return max(maxSubArrSum, abs(minSubArrSum));
+    }
+};
+
+
+// Time Complexity ->O(n)
+// Space Complexity -> O(1)
 
 // =========================================================================================
