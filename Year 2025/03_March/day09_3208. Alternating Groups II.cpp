aa79cce4f2c4629703f0ab5d5437832177ecdd2f
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-03-09 12:51:47 -0700

    March Day - 09 solved: 3208. Alternating Groups II (Approach -2)

--- Year 2025/03_March/day09_3208. Alternating Groups II.cpp
+++ Year 2025/03_March/day09_3208. Alternating Groups II.cpp
@@ -34,3 +34,42 @@ public:
 // SPace Complexity -> O(1)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        int N = n + (k - 1);
+        int groupCount = 0;
+
+        for(int i = 0; i < k - 1; i++){
+            colors.push_back(colors[i]);
+        }
+
+        int startIdx = 0, endIdx = 1;
+        while(endIdx < N){
+            if(colors[endIdx] == colors[endIdx - 1]){
+                startIdx = endIdx;
+                endIdx++;
+                continue;
+            }
+
+            if(endIdx - startIdx + 1 == k){
+                groupCount++;
+                startIdx++;
+            }
+            endIdx++;
+        }
+
+        return groupCount;
+    }
+};
+
+
+// Time Complexity -> O(n + k)
+// SPace Complexity -> O(1)
+
+// =========================================================================================
