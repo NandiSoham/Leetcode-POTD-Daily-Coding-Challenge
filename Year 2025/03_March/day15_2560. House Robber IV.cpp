c4b4192391884c53b3ff284c96fc22822413cd44
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-03-15 13:04:57 -0700

    March Day - 15 solved: 2560. House Robber IV (Approach - 1)

---
+++ Year 2025/03_March/day15_2560. House Robber IV.cpp
@@ -0,0 +1,38 @@
+// Problem Link -> https://leetcode.com/problems/house-robber-iv/description/
+
+// ======================================= Approach - 1 (Brute Force - TLE) ====================================
+
+class Solution {
+public:
+    int n;
+    int findMinCapability(vector<int>& nums, int idx, int remaining, vector<vector<int>>& memo) {
+        if(remaining == 0) {
+            return 0;
+        }
+
+        if(idx >= n) {
+            return INT_MAX;
+        }   
+
+        if(memo[idx][remaining] != -1) {
+            return memo[idx][remaining];
+        }
+
+        int take = max(nums[idx], findMinCapability(nums, idx + 2, remaining - 1, memo));
+
+        int skip = findMinCapability(nums, idx + 1, remaining, memo);
+
+        return memo[idx][remaining] = min(take, skip);
+    }
+
+    int minCapability(vector<int>& nums, int k) {
+        n = nums.size();
+        vector<vector<int>> memo(n + 1, vector<int>(k + 1, -1));
+        return findMinCapability(nums, 0, k, memo);
+    }
+};
+
+// Time Complexity -> O(n * k)
+// Space Complexity -> O(n * k)
+
+// =============================================================================================================
