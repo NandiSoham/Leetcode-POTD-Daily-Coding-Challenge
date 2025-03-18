384a2cd7ad5b4edff687a44730c31ee8286acb08
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-03-18 00:59:39 -0700

    March Day - 18 solved: 2401. Longest Nice Subarray (Approach - 2)

--- Year 2025/03_March/day18_2401. Longest Nice Subarray.cpp
+++ Year 2025/03_March/day18_2401. Longest Nice Subarray.cpp
@@ -38,4 +38,36 @@ public:
 // Time Complexity -> O(n ^ 3)
 // Space Complexity -> O(1)
 
-// =============================================================================================================
+// =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++){
+            int bitMask = 0;
+
+            for(int j = i; j < n; j++){
+                if((bitMask & nums[j]) != 0){
+                    break;
+                }
+                
+                bitMask = (bitMask | nums[j]);
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+
+        return maxLen;
+    }
+};
+
+
+// Time Complexity -> O(n ^ 2)
+// Space Complexity -> O(1)
+
+// =========================================================================================
