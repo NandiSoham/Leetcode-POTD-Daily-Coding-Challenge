ca2bb87ade356e66f4f57854fe356200db62a942
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-03-25 23:40:30 -0700

    Create day26_2033. Minimum Operations to Make a Uni-Value Grid.cpp

---
+++ Year 2025/03_March/day26_2033. Minimum Operations to Make a Uni-Value Grid.cpp
@@ -0,0 +1,33 @@
+// problem Link -> https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/
+
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int stepSize) {
+        int rowCount = grid.size();
+        int colCount = grid[0].size();
+
+        vector<int> elements;
+        
+        for(int i = 0; i < rowCount; i++) {
+            for(int j = 0; j < colCount; j++) {
+                elements.push_back(grid[i][j]);
+            }
+        }
+
+        int totalElements = elements.size();
+
+        nth_element(elements.begin(), elements.begin() + totalElements / 2, elements.end());
+
+        int medianVal = elements[totalElements / 2];
+        int operationsCount = 0;
+
+        for(int &num : elements) {
+            if(num % stepSize != medianVal % stepSize) {
+                return -1;
+            }
+            operationsCount += abs(medianVal - num) / stepSize;
+        }
+
+        return operationsCount;
+    }
+};
