03d7fff6b86341dacb029ae9fed300e8948c0183
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-03-26 23:41:20 -0700

    March Day - 27 solved: 2780. Minimum Index of a Valid Split (Approach - 2)

--- Year 2025/03_March/day27_2780. Minimum Index of a Valid Split.cpp
+++ Year 2025/03_March/day27_2780. Minimum Index of a Valid Split.cpp
@@ -37,3 +37,57 @@ public:
 // Space Complexity -> O(n)
 
 // =========================================================================================
+
+
+// ======================================= Approach - 2 ====================================
+
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        int majorityElement = -1;
+        int majorityCount = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(majorityCount == 0) {
+                majorityElement = nums[i];
+                majorityCount = 1;
+            } else if(nums[i] == majorityElement) {
+                majorityCount++;
+            } else {
+                majorityCount--;
+            }
+        }
+
+        int totalMajorityCount = 0;
+        for(int &num : nums) {
+            if(num == majorityElement) {
+                totalMajorityCount++;
+            }
+        }
+
+        majorityCount = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == majorityElement) {
+                majorityCount++;
+            }
+
+            int remainingMajorityCount = totalMajorityCount - majorityCount;
+            int leftSize = i + 1;
+            int rightSize = n - i - 1;
+
+            if(majorityCount * 2 > leftSize && remainingMajorityCount * 2 > rightSize) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+// =========================================================================================
