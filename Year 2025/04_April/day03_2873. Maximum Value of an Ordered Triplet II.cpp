2a713f69cef07a214bfd229faf21df27702a0916
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-04-02 23:29:37 -0700

    April Day - 03 solved: 2873. Maximum Value of an Ordered Triplet II

---
+++ Year 2025/04_April/day03_2873. Maximum Value of an Ordered Triplet II.cpp
@@ -0,0 +1,25 @@
+// Problem Link -> https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/description/
+
+class Solution {
+    #define ll long long
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        ll maxProduct = 0;
+        ll maxDifference = 0;
+        ll maxLeftValue = 0;
+
+        for(int i = 0; i < n; ++i){
+            maxProduct = max<ll>(maxProduct, 1LL * maxDifference * nums[i]);
+            maxDifference = max<ll>(maxDifference, (maxLeftValue - nums[i]));
+            maxLeftValue = max<ll>(maxLeftValue, nums[i]);
+        }
+        
+        return maxProduct;
+    }
+};
+
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
