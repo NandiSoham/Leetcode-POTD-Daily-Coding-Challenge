43921ad86beac08afd8d8eb226a624d12865597f
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-04-27 00:38:56 -0700

    April Day - 27 solved: 3392. Count Subarrays of Length Three With a Condition

---
+++ Year 2025/04_April/day27_3392. Count Subarrays of Length Three With a Condition.cpp
@@ -0,0 +1,21 @@
+// Problem Link -> https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/description/
+
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for(int i = 1; i < n - 1; i++){
+            if(nums[i] == (nums[i - 1] + nums[i + 1]) * 2){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
+// Time complexity -> O(n)
+// Space complexity -> O(1)
