1a785e8e05224dea3d466e76e42c90c55c4a2386
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-06-02 10:14:24 -0700

    June Day - 02 Solved: 135. Candy (Approach - 2)

--- Year 2025/06_June/day02_135. Candy.cpp
+++ Year 2025/06_June/day02_135. Candy.cpp
@@ -35,3 +35,47 @@ public:
 // Space Complexity -> O(n)
 
 // ==========================================================================================
+
+
+// ====================================== Approach - 1 ======================================
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int totalCandies = n;
+
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i - 1]){
+                i++;
+                continue;
+            }
+
+            int peak = 0;
+            while(ratings[i] > ratings[i - 1]){
+                peak++;
+                totalCandies += peak;
+                i++;
+
+                if(i == n) return totalCandies;
+            }
+
+            int dip = 0;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                dip++;
+                totalCandies += dip;
+                i++;
+            }
+            totalCandies -= min(peak, dip);
+        }
+
+        return totalCandies;
+    }
+};
+
+
+// Time COmplexity -> O(n)
+// Space Complexity -> O(1)
+
+// ==========================================================================================
