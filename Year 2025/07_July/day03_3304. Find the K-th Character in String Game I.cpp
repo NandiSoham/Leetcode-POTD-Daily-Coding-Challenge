e43c830a9044b2c2f427851bcb69450ab7264dcb
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-07-03 09:38:42 -0700

    July Day - 03 Solved: 3304. Find the K-th Character in String Game I (in smarter way of code writing)

--- Year 2025/07_July/day03_3304. Find the K-th Character in String Game I.cpp
+++ Year 2025/07_July/day03_3304. Find the K-th Character in String Game I.cpp
@@ -1,4 +1,4 @@
-// Problem link -> 
+// Problem link -> https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/description/
 
 class Solution {
 public:
@@ -15,6 +15,27 @@ public:
                 nextChar = word[i] + 1;
 
                 word.push_back(nextChar);
+            }
+        }
+
+        return word[k - 1];
+    }
+};
+
+
+// ======================= Alternate/ Smarter Way to write the same logic =================
+
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+
+        while(word.length() < k){
+            int n = word.length();
+
+            for(int i = 0; i < n; i++){
+                char ch = word[i] == 'z' ? 'a' : word[i]+1;
+                word.push_back(ch);
             }
         }
 
