50561585232ffddc8833607219267b4686869fdb
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-07-21 09:48:28 -0700

    July Day - 21 Solved: 1957. Delete Characters to Make Fancy String (Approach - 2)

--- Year 2025/07_July/day21_1957. Delete Characters to Make Fancy String.cpp
+++ Year 2025/07_July/day21_1957. Delete Characters to Make Fancy String.cpp
@@ -30,3 +30,25 @@ public:
 // Space Complexity -> O(1)  not considering and ans string
 
 // ==========================================================================
+
+
+// =============================== Approach - 2 =============================
+
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n = s.length();
+        if(n < 3) return s;
+
+        int j = 2;
+        for(int i = 2; i < n; i++){
+            if(s[i] != s[j - 1] || s[i] != s[j - 2]){
+                s[j] = s[i];
+                j++;
+            }
+        }
+
+        s.resize(j);
+        return s;
+    }
+};
