fd78f5b38ef632b15376409742e881142e771318
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-07-23 12:06:04 -0700

    July Day - 23 Solved: 1717. Maximum Score From Removing Substrings

---
+++ Year 2025/07_July/day23_1717. Maximum Score From Removing Substrings.cpp
@@ -0,0 +1,43 @@
+// Problem link -> https://leetcode.com/problems/maximum-score-from-removing-substrings/description/
+
+class Solution {
+public:
+    string removeSubstring(string& s, string& substring) {
+        stack<char> strStack;
+        for (char& ch : s) {
+            if (ch == substring[1] && !strStack.empty() &&
+                strStack.top() == substring[0]) {
+                strStack.pop();
+            } else strStack.push(ch);
+        }
+
+        string temp;
+        while (!strStack.empty()) {
+            temp.push_back(strStack.top());
+            strStack.pop();
+        }
+        reverse(temp.begin(), temp.end());
+        return temp;
+    }
+
+    int maximumGain(string s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+        string greaterValuedStr = (x > y) ? "ab" : "ba";
+        string lesserValuedStr = (x > y) ? "ba" : "ab";
+
+        string firstPassTemp = removeSubstring(s, greaterValuedStr);
+        int countRemovedPair = (n - firstPassTemp.length()) / 2;
+        score += countRemovedPair * max(x, y);
+
+        string secondPassTemp = removeSubstring(firstPassTemp, lesserValuedStr);
+        countRemovedPair = (firstPassTemp.length() - secondPassTemp.length()) / 2;
+        score += countRemovedPair * min(x, y);
+
+        return score;
+    }
+};
+
+
+// Time COmplexity -> O(n)
+// Space Complexity -> O(n)
