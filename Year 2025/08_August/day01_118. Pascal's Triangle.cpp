e02ebbe9e35538d2108873cf5d26dcd43bb34b58
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-08-01 10:28:21 -0700

    August Day - 01 Solved: 118. Pascal's Triangle

--- Year 2025/08_August/day01_118. Pascal's Triangle.cpp
+++ Year 2025/08_August/day01_118. Pascal's Triangle.cpp
@@ -1,1 +1,22 @@
+// Problem Link -> https://leetcode.com/problems/pascals-triangle/description/
 
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+
+        for(int i = 0; i < numRows; i++){
+            ans[i] = vector<int> (i+1, 1);
+
+            for(int j = 1; j < i; j++){
+                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(n^2)
