f765ea91dc25de19b374419dc7b6b7456bb22b66
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-08-05 10:34:52 -0700

    August Day - 05 Solved: 3477. Fruits Into Baskets II

---
+++ Year 2025/08_August/day05_3477. Fruits Into Baskets II.cpp
@@ -0,0 +1,25 @@
+// Problem Link -> https://leetcode.com/problems/fruits-into-baskets-ii/description/
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        int placedCount = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++) {
+                if(fruits[i] <= baskets[j]){
+                    placedCount++;
+                    baskets[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return n - placedCount;
+    }
+};
+
+
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(1)
