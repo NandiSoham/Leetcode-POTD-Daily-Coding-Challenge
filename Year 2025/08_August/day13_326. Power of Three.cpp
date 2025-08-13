eba4472655a12cb1df554360f5bb3426ac8b0810
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-08-13 10:17:17 -0700

    August Day - 13 Solved: 326. Power of Three (Approach - 3)

--- Year 2025/08_August/day13_326. Power of Three.cpp
+++ Year 2025/08_August/day13_326. Power of Three.cpp
@@ -42,3 +42,21 @@ public:
 // Time Complexity -> O(log3 n)
 // Space Complexity -> O(1)
 // ====================================================================================
+
+
+// =================================== Approach - 3 ===================================
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        double x = (log10(n) / log10(3));
+
+        return x == int(x);
+    }
+};
+
+
+// Time Complexity -> O(1)
+// Space Complexity -> O(1)
+// ====================================================================================
