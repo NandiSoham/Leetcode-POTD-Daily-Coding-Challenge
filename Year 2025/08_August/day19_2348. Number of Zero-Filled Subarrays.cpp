52462b36c7da01053df0e72a1a2f7eb13baac4a9
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-08-19 11:55:32 -0700

    August Day - 19 Solved: 2348. Number of Zero-Filled Subarrays (Approach - 1)

---
+++ Year 2025/08_August/day19_2348. Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,32 @@
+// Problem Link -> https://leetcode.com/problems/number-of-zero-filled-subarrays/description/
+
+// ============================== Approach - 1 ==============================
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+        int idx = 0;
+
+        while(idx < n){
+            long long zerosCount = 0;
+
+            if(nums[idx] == 0){
+                while(idx < n && nums[idx] == 0){
+                    idx++;
+                    zerosCount++;
+                }
+            } else idx++;
+
+            ans += (zerosCount * (zerosCount + 1)) / 2;
+        }
+        
+        return ans;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+// ==========================================================================
