1d35748a48f2d99f1f24dfddb91db9f4f718ab20
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-08-19 12:05:06 -0700

    August Day - 19 Solved: 2348. Number of Zero-Filled Subarrays (Approach - 2)

--- Year 2025/08_August/day19_2348. Number of Zero-Filled Subarrays.cpp
+++ Year 2025/08_August/day19_2348. Number of Zero-Filled Subarrays.cpp
@@ -30,3 +30,31 @@ public:
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
 // ==========================================================================
+
+
+// ============================== Approach - 2 ==============================
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count = 0;
+        int contZero = 0;
+        int n = nums.size();
+        
+        for (int it : nums) {
+            if(it == 0){
+                contZero++;
+            } else {
+                contZero = 0;
+            }
+            count += contZero;
+        }
+        
+        return count;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+// ==========================================================================
