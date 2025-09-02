b39a9bf9f3e74caf5b939f1b1888946750fea557
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-09-02 11:04:26 -0700

    September Day - 02 Solved: 3025. Find the Number of Ways to Place People I (Approach - 2)

--- Year 2025/09_September/day02_3025. Find the Number of Ways to Place People I.cpp
+++ Year 2025/09_September/day02_3025. Find the Number of Ways to Place People I.cpp
@@ -51,3 +51,48 @@ public:
 // Space complexity -> O(1)
 
 // ==============================================================================
+
+
+// ============================== Approach - 2 ==================================
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& coords) {
+        int n = coords.size();
+
+        sort(coords.begin(), coords.end(), [](vector<int>& p1, vector<int>& p2) {
+            return (p1[0] != p2[0]) ? p1[0] < p2[0] : p1[1] > p2[1];
+        });
+        
+        int pairCount = 0;
+        
+        for (int i = 0; i < n; ++i) {
+
+            int leftX = coords[i][0];
+            int topY  = coords[i][1];
+            
+            int lowestYSeen = INT_MIN;  
+            
+            for (int j = i + 1; j < n; ++j) {
+
+                int rightX = coords[j][0];
+                int bottomY = coords[j][1];
+                
+                if (bottomY > topY) continue;
+                
+                if (bottomY > lowestYSeen) {
+                    ++pairCount;
+                    lowestYSeen = bottomY;
+                }
+            }
+        }
+        
+        return pairCount;
+    }
+};
+
+
+// Time Complexity -> O(n^2)
+// Space complexity -> O(1)
+
+// ==============================================================================
