a24979a89b519dd6e9199a56c4e9b809d7e77ea0
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-09-05 10:43:52 -0700

    September Day - 05 Solved: 2749. Minimum Operations to Make the Integer Zero

---
+++ Year 2025/09_September/day05_2749. Minimum Operations to Make the Integer Zero.cpp
@@ -0,0 +1,35 @@
+// Problem Link -> https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero/description/
+
+class Solution {
+public:
+    int setBitCount(long long x) {
+        int count = 0;
+        while (x > 0) {
+            if (x & 1) count++;
+            x >>= 1;
+        }
+        return count;
+    }
+
+    int makeTheIntegerZero(int num1, int num2) {
+        long long t = 0;
+
+        while (true) {
+            long long val = (long long)num1 - t * (long long)num2;
+
+            if (val < 0) return -1;
+
+            if (setBitCount(val) <= t && t <= val) {
+                return (int)t;
+            }
+
+            t++;
+        }
+
+        return -1;
+    }
+};
+
+
+// Time Complexity -> O()
+// Space Complexity -> O()
