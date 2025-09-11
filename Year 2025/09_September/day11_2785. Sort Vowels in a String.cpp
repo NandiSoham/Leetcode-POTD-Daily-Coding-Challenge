aa828b789ab9c47eff50e7e316b1a782a382c363
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-09-11 10:16:35 -0700

    September Day - 11 Solved: 2785. Sort Vowels in a String (Approach - 2)

--- Year 2025/09_September/day11_2785. Sort Vowels in a String.cpp
+++ Year 2025/09_September/day11_2785. Sort Vowels in a String.cpp
@@ -39,3 +39,48 @@ public:
 // Space Complexity -> O(1)
 
 // ================================================================================
+
+
+
+// ================================= Approach - 2 =================================
+
+class Solution {
+public:
+
+    bool isVowel(char ch){
+        ch = tolower(ch);
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+
+    string sortVowels(string s) {
+        int n = s.length();
+        unordered_map<char, int> vowelMap;
+        string vowelStr = "AEIOUaeiou";
+
+        for(char &ch : s){
+            if(isVowel(ch)){
+                vowelMap[ch]++;
+            }
+        }
+
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(isVowel(s[i])){
+                while(vowelMap[vowelStr[j]] == 0){
+                    j++;
+                }
+
+                s[i] = vowelStr[j];
+                vowelMap[vowelStr[j]]--;
+            }
+        }
+
+        return s;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+// ================================================================================
