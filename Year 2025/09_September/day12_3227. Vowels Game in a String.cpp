5aa9872944138435ac1cbb008d1bc7f5f59bfd2a
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-09-12 08:13:32 -0700

    September Day - 12 Solved: 3227. Vowels Game in a String

---
+++ Year 2025/09_September/day12_3227. Vowels Game in a String.cpp
@@ -0,0 +1,19 @@
+// Problem Link -> https://leetcode.com/problems/vowels-game-in-a-string/description/
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+
+        for(char &ch : s) {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
