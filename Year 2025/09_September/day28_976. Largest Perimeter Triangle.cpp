7016d39973a373b07ddfd719d39cc02c0eed95c8
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-09-28 10:13:54 -0700

    September day - 28 Solved: 976. Largest Perimeter Triangle

---
+++ Year 2025/09_September/day28_976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,22 @@
+// Problem Link -> https://leetcode.com/problems/largest-perimeter-triangle/description/
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));  
+
+        for (int i = n - 3; i >= 0; i--) {
+            
+            if (nums[i] + nums[i + 1] > nums[i + 2]){
+                return nums[i] + nums[i + 1] + nums[i + 2];  
+            }
+        }
+
+        return 0;  
+    }
+};
+
+// Time Complexity -> O(n log n)
+// Space Complexity -> O(1)
