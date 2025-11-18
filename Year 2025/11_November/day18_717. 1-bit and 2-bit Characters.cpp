49e361218910409af0e2dd8bfd60b889990c43e6
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2025-11-18 09:31:09 -0800

    Add solution for 1-bit and 2-bit characters
    Implement solution for 1-bit and 2-bit characters problem.

---
+++ Year 2025/11_November/day18_717. 1-bit and 2-bit Characters.cpp
@@ -0,0 +1,19 @@
+// Problem link -> https://leetcode.com/problems/1-bit-and-2-bit-characters/description/
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int i = 0;
+
+        while(i < n-1) {
+            i += (bits[i] == 1) ? 2 : 1;
+        }
+
+        return i == n-1;
+    }
+};
+
+
+// Time Complexity -> O(n)
+// Spaec Complexity -> O(1)
